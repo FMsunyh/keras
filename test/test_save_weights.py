64fdc0494963e433d0837518dcf3a45288e57188
fmsunyh <fmsunyh@gmail.com>
2018-07-09 15:52:36 +0800

    debug help

--- test/test_save_weights.py
+++ test/test_save_weights.py
@@ -1,5 +1,6 @@
+from keras.layers.convolutional import Convolution2D, MaxPooling2D
 from keras.models import Sequential
-from keras.layers.core import Dense, Dropout, Activation
+from keras.layers.core import Dense, Dropout, Activation, Flatten
 from keras.optimizers import SGD
 
 import sys
@@ -7,17 +8,43 @@ sys.setrecursionlimit(10000) # to be able to pickle Theano compiled functions
 
 import pickle, numpy
 
+# def create_model():
+#     model = Sequential()
+#     model.add(Dense(256, 2048, init='uniform', activation='relu'))
+#     model.add(Dropout(0.5))
+#     model.add(Dense(2048, 2048, init='uniform', activation='relu'))
+#     model.add(Dropout(0.5))
+#     model.add(Dense(2048, 2048, init='uniform', activation='relu'))
+#     model.add(Dropout(0.5))
+#     model.add(Dense(2048, 2048, init='uniform', activation='relu'))
+#     model.add(Dropout(0.5))
+#     model.add(Dense(2048, 256, init='uniform', activation='linear'))
+#     return model
+
 def create_model():
     model = Sequential()
-    model.add(Dense(256, 2048, init='uniform', activation='relu'))
+    model.add(Convolution2D(32, 3, 3, 3, border_mode='full'))
+    model.add(Activation('relu'))
+    model.add(Convolution2D(32, 32, 3, 3))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(poolsize=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Convolution2D(64, 32, 3, 3, border_mode='full'))
+    model.add(Activation('relu'))
+    model.add(Convolution2D(64, 64, 3, 3))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(poolsize=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Flatten())
+    model.add(Dense(64 * 8 * 8, 512, init='normal'))
+    model.add(Activation('relu'))
     model.add(Dropout(0.5))
-    model.add(Dense(2048, 2048, init='uniform', activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(2048, 2048, init='uniform', activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(2048, 2048, init='uniform', activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(2048, 256, init='uniform', activation='linear'))
+
+    model.add(Dense(512, 20, init='normal'))
+    model.add(Activation('softmax'))
+
     return model
 
 model = create_model()
